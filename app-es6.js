fbe658263e00ef1eadde0ce737622a94d72a875d
Will Schmidt <will.schmidt@gmail.com>
2019-10-04 07:46:17 +1300

    Add local storage functionality

--- app-es6.js
+++ app-es6.js
@@ -54,6 +54,55 @@ class UI {
   }
 }
 
+// Local storage
+class Store {
+  static getBooks() {
+    let books
+    if (localStorage.getItem('books') === null) {
+      books = []
+    } else {
+      books = JSON.parse(localStorage.getItem('books'))
+    }
+
+    return books
+  }
+
+  static displayBooks() {
+    const books = Store.getBooks()
+
+    books.forEach(function(book) {
+      const ui = new UI()
+
+      // Add book to list
+
+      ui.addBookToList(book)
+    })
+  }
+
+  static addBook(book) {
+    const books = Store.getBooks()
+
+    books.push(book)
+
+    localStorage.setItem('books', JSON.stringify(books))
+  }
+
+  static removeBook(isbn) {
+    const books = Store.getBooks()
+
+    books.forEach(function(book, index) {
+      if (book.isbn === isbn) {
+        books.splice(index, 1)
+      }
+    })
+
+    localStorage.setItem('books', JSON.stringify(books))
+  }
+}
+
+// DOM load event
+document.addEventListener('DOMContentLoaded', Store.displayBooks())
+
 // Event listener for add book
 document.getElementById('book-form').addEventListener('submit', function(e) {
   // Get form values
@@ -75,6 +124,9 @@ document.getElementById('book-form').addEventListener('submit', function(e) {
     // ADD BOOK to list
     ui.addBookToList(book)
 
+    // Add book to local storage
+    Store.addBook(book)
+
     // Show success
     ui.showAlert('Book added!', 'success')
 
@@ -95,6 +147,8 @@ document.getElementById('book-list').addEventListener('click', function(e) {
   // Delete book
   ui.deleteBook(e.target)
 
+  // Remove from local storage
+  Store.removeBook(e.target.parentElement.previousElementSibling.textContent)
   // Show message
   ui.showAlert('Book removed!', 'success')
 
